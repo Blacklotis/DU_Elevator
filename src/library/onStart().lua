e179af252d12824c2959a99a665907cd5789f0f2
David Diller <david.diller1@gmail.com>
2022-10-21 20:28:50 -0700

    conformity

--- src/library/onStart().lua
+++ src/library/onStart().lua
@@ -119,64 +119,58 @@ function getHeading(forward) -- code provided by tomisunlucky
     return angle
 end
 
+--  Provides methods for transforming between geometric representations.
 
+--  Methods:
+--    Transform.computeHeading - shown by a compass when on a planet.
+--    Transform.computePRYangles - to determine 'pitch', 'roll', and 'yaw'.
+--    Transform.makeCoordinateConverters - between world and construct coordinates
 
+--  Description
+--    DualUniverse (DU) defines several coordinate systems. This module deals with
+--    the conversion and use of 'world coordinates' (a cartesian coordinate
+--    system that spans a planetary system) and 'construct coordinates' which
+--    is the coordinate system defined by a 'core' element (when not overriden
+--    by a 'gyro' unit).
+--    A 'core' element orientates the coordinate system with the axis: 'right',
+--    'forward' (the direction indicated by the arrow on the core), and 'up'
+--    (indicated by the side of the core with the arrow). This coordinate system
+--    is a 'right-handed' coordinate system so 'right' is the direction pointed
+--    to by the thumb on the right hand when the fingers "curl" from 'forward'
+--    direction to the 'up' direction.
 
+--    Note that the routines in this module accept input vectors of type
+--    'cpml.vec3' or as a three element Lua array (as returned by DU API codex
+--    functions).  All returned vectors are of type 'cpml.vec3'
 
+--    The 'computeHeading' function takes a position (in world coordinates),
+--    a direction vector and the world coordinates of the planet's center and
+--    returns the "compass heading" in radians coresponding to the direction
+--    vector. "North" has a heading of 0 radians.
 
---[[ 
-  Provides methods for transforming between geometric representations.
+--    The 'computePRYangles' function returns the "pitch", "roll", and "yaw"
+--    angles (in radians) which could be used to rotate a one coordinate system
+--    so that it aligns with another. These are equivalent (not equal) to Euler
+--    angles.
 
-  Methods:
-    Transform.computeHeading - shown by a compass when on a planet.
-    Transform.computePRYangles - to determine 'pitch', 'roll', and 'yaw'.
-    Transform.makeCoordinateConverters - between world and construct coordinates
+--    The 'makeCoordinateConverters() function returns two functions and three
+--    vectors. The first function will convert a position in "world" coordinates
+--    to the same position in "construct" coordinates. 
+--    The second function returned converts a position in "construct" coordinates
+--    to the same position in "world coordinates".
+--    Finally, the three vectors define the 'right', 'forward', and 'up' axis
+--    of the construct coordinate system in terms of the world coordinates.
 
-  Description
-    DualUniverse (DU) defines several coordinate systems. This module deals with
-    the conversion and use of 'world coordinates' (a cartesian coordinate
-    system that spans a planetary system) and 'construct coordinates' which
-    is the coordinate system defined by a 'core' element (when not overriden
-    by a 'gyro' unit).
-    A 'core' element orientates the coordinate system with the axis: 'right',
-    'forward' (the direction indicated by the arrow on the core), and 'up'
-    (indicated by the side of the core with the arrow). This coordinate system
-    is a 'right-handed' coordinate system so 'right' is the direction pointed
-    to by the thumb on the right hand when the fingers "curl" from 'forward'
-    direction to the 'up' direction.
+--  Example Usage:
 
-    Note that the routines in this module accept input vectors of type
-    'cpml.vec3' or as a three element Lua array (as returned by DU API codex
-    functions).  All returned vectors are of type 'cpml.vec3'
-
-    The 'computeHeading' function takes a position (in world coordinates),
-    a direction vector and the world coordinates of the planet's center and
-    returns the "compass heading" in radians coresponding to the direction
-    vector. "North" has a heading of 0 radians.
-
-    The 'computePRYangles' function returns the "pitch", "roll", and "yaw"
-    angles (in radians) which could be used to rotate a one coordinate system
-    so that it aligns with another. These are equivalent (not equal) to Euler
-    angles.
-
-    The 'makeCoordinateConverters() function returns two functions and three
-    vectors. The first function will convert a position in "world" coordinates
-    to the same position in "construct" coordinates. 
-    The second function returned converts a position in "construct" coordinates
-    to the same position in "world coordinates".
-    Finally, the three vectors define the 'right', 'forward', and 'up' axis
-    of the construct coordinate system in terms of the world coordinates.
-
-  Example Usage:
-
-    Transform = require('autoconf.custom.transform')
+--    Transform = require('autoconf.custom.transform')
 
     -- On Alioth, determine the direction a construct is heading (NOT facing).
-    alioth = vec3({x=-8.000,y=-8.000,z=-126303.000}) -- center position
-    heading = Transform.computeHeading(alioth,
-                                       core.getConstructWorldPos(),
-                                       core.getWorldVelocity())
-    system.print('Heading: ' .. 180*heading/math.pi .. ' degrees from north')
+--    alioth = vec3({x=-8.000,y=-8.000,z=-126303.000}) -- center position
+--    heading = Transform.computeHeading(alioth,
+--                                       core.getConstructWorldPos(),
+--                                       core.getWorldVelocity())
+--    system.print('Heading: ' .. 180*heading/math.pi .. ' degrees from north')
 
     -- Find the PRY angles of a construct relative to a coordinate system of
     -- a construct that is level (i.e. 'up' is colinear with "vertical") and
@@ -185,44 +179,40 @@ end
     -- the world coordinate system:
 
     -- First define the reference coordinate system's axis:
-    velocity   = vec3(core.getWorldVelocity())
-    refUp      = -vec3(core.getWorldVertical())
-    refForward = velocity:project_on_plane(refUp):normalize()
+--    velocity   = vec3(core.getWorldVelocity())
+--    refUp      = -vec3(core.getWorldVertical())
+--    refForward = velocity:project_on_plane(refUp):normalize()
     -- if needed, refRight is given by: refForward:cross(refUp)
 
     -- Now get the construct's 'forward' and 'up' axis:
-    fwd        = core.getConstructWorldOrientationForward()
-    up         = core.getConstructWorldOrientationUp()
+--    fwd        = core.getConstructWorldOrientationForward()
+--    up         = core.getConstructWorldOrientationUp()
 
-    pitch,roll,yaw = Transform.computePRYangles(refForward, refUp, fwd, up)
-    system.print('Pitch: ' .. 180*pitch/math.pi ..
-                 ' roll: ' .. 180*roll/math.pi ..
-                  ' yaw: ' .. 180*yaw/math.pi)
+--    pitch,roll,yaw = Transform.computePRYangles(refForward, refUp, fwd, up)
+--    system.print('Pitch: ' .. 180*pitch/math.pi ..
+--                 ' roll: ' .. 180*roll/math.pi ..
+--                  ' yaw: ' .. 180*yaw/math.pi)
 
     -- In some cases, the DU API provides functions to get the same vector
     -- in either world or construct coordinate systems. The following compares
     -- the native methods and the conversion results for one case:
 
-    velocityWorld = vec3(core.getWorldVelocity())
-    velocityCref  = vec3(core.getVelocity())
+--    velocityWorld = vec3(core.getWorldVelocity())
+--    velocityCref  = vec3(core.getVelocity())
     
-    worldToCref, crefToWorld = Transform.makeCoordinateConverters(core)
+--    worldToCref, crefToWorld = Transform.makeCoordinateConverters(core)
 
-    error1 = (velocityCref  - worldToCref(velocityWorld)):len()
-    error2 = (velocityWorld - crefToWorld(velocityCref)):len()
-    system,print('differences: ' .. error1 .. ', ' .. error2)
+--    error1 = (velocityCref  - worldToCref(velocityWorld)):len()
+--    error2 = (velocityWorld - crefToWorld(velocityCref)):len()
+--    system,print('differences: ' .. error1 .. ', ' .. error2)
                  
-  Also See: planetref.lua
-]]--
-
+--  Also See: planetref.lua
 
 local vec3  = require('cpml.vec3')
 local utils = require('cpml.utils')
 local clamp = utils.clamp
 
-
 Transform = {}
-
 --
 -- computeHeading - compute compass heading corresponding to a direction.
 -- planetCenter[in]: planet's center in world coordinates.
@@ -281,7 +271,6 @@ end
 -- 
 
 if library and library.systemResolution3 then
-
 -- systemResolution3 "solves" the equation M*x = c0 by finding the inverse
 -- of 'M' so that x = M(inv)*c0. Since a native matrix multiplication routine
 -- is not provided (shame), this fact is used to effect one by finding an 'M'
@@ -293,45 +282,41 @@ if library and library.systemResolution3 then
 -- Note: I have NOT benchmarked the following two implementations to see which
 -- is faster, the presumption is the native version should be so it is used
 -- when available.
-local solve = library.systemResolution3
+    local solve = library.systemResolution3
 
-function Transform.makeCoordinateConverters(coreUnit)
-    local core     = coreUnit or core
-    local vc1      = core.getConstructWorldOrientationRight()
-    local vc2      = core.getConstructWorldOrientationForward()
-    local vc3      = core.getConstructWorldOrientationUp()
-    local vc1t     = solve(vc1, vc2, vc3, {1,0,0})
-    local vc2t     = solve(vc1, vc2, vc3, {0,1,0})
-    local vc3t     = solve(vc1, vc2, vc3, {0,0,1})
-    return  function(world) -- transform to construct coordinates
-                world = world.x and {world:unpack()} or world
-                return vec3(solve(vc1, vc2, vc3, world))
-            end,
-            function(cref) -- transform to world coordinates
-                cref = cref.x and {cref:unpack()} or cref
-                return vec3(solve(vc1t, vc2t, vc3t, cref))
-            end,
-            vec3(vc1), vec3(vc2), vec3(vc3)
-end
-
+    function Transform.makeCoordinateConverters(coreUnit)
+        local core     = coreUnit or core
+        local vc1      = core.getConstructWorldOrientationRight()
+        local vc2      = core.getConstructWorldOrientationForward()
+        local vc3      = core.getConstructWorldOrientationUp()
+        local vc1t     = solve(vc1, vc2, vc3, {1,0,0})
+        local vc2t     = solve(vc1, vc2, vc3, {0,1,0})
+        local vc3t     = solve(vc1, vc2, vc3, {0,0,1})
+        return  function(world) -- transform to construct coordinates
+                    world = world.x and {world:unpack()} or world
+                    return vec3(solve(vc1, vc2, vc3, world))
+                end,
+                function(cref) -- transform to world coordinates
+                    cref = cref.x and {cref:unpack()} or cref
+                    return vec3(solve(vc1t, vc2t, vc3t, cref))
+                end,
+                vec3(vc1), vec3(vc2), vec3(vc3)
+    end
 else
-
-function Transform.makeCoordinateConverters(coreUnit)
-    local core     = coreUnit or core
-    local right    = vec3(core.getConstructWorldOrientationRight())
-    local fwd      = vec3(core.getConstructWorldOrientationForward())
-    local up       = vec3(core.getConstructWorldOrientationUp())
-    return  function(world) -- transform to construct coordinates
-                world = world.x and world or vec3(world)
-                return vec3(right:dot(world), fwd:dot(world), up:dot(world))
-            end,
-            function(cref) -- transform to world coordinates
-                cref = cref.x and cref or vec3(cref)
-                return right*cref.x + fwd*cref.y + up*cref.z
-            end,
-            right, fwd, up
+    function Transform.makeCoordinateConverters(coreUnit)
+        local core     = coreUnit or core
+        local right    = vec3(core.getConstructWorldOrientationRight())
+        local fwd      = vec3(core.getConstructWorldOrientationForward())
+        local up       = vec3(core.getConstructWorldOrientationUp())
+        return  function(world) -- transform to construct coordinates
+                    world = world.x and world or vec3(world)
+                    return vec3(right:dot(world), fwd:dot(world), up:dot(world))
+                end,
+                function(cref) -- transform to world coordinates
+                    cref = cref.x and cref or vec3(cref)
+                    return right*cref.x + fwd*cref.y + up*cref.z
+                end,
+                right, fwd, up
+    end
 end
-
-end
-
 return Transform
